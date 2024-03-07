0b2a3181968eb7077676c0d9bf57b940f5fbec4d
Yating Liu <13189092548@163.com>
2024-03-07 15:24:50 -0500

    modify

--- topic_matrix_estimation_procedure.R
+++ topic_matrix_estimation_procedure.R
@@ -3,15 +3,16 @@ library('rARPACK')
 library(roxygen2)
 library(quadprog)
 
-source("r/vertex_hunting_functions.R")
-source("r/simplex_dist.R")
+source("VH_algo.R")
+source("core_recovery.R")
 library(Matrix)
+
 
 
 score <- function(D, K, scatterplot=FALSE, K0=NULL, m=NULL, M=NULL, threshold=FALSE,
                   Mquantile=0.00, VHMethod = 'SP', normalize="none",
-                  alpha=0.5, max_K=150, returnW=FALSE, estimateK=TRUE,
-                  as.sparse = TRUE){
+                  alpha=0.5, max_K=150, returnW=FALSE, estimateK=FALSE,
+                  as.sparse = FALSE){
   #' This function computes the estimates for the A and W matrix based on the algorithm proposed in Ke and Wang's work: https://arxiv.org/pdf/1704.07016.pdf
   #'
   #'
@@ -177,8 +178,7 @@ score <- function(D, K, scatterplot=FALSE, K0=NULL, m=NULL, M=NULL, threshold=FA
   #Step 5
   if (normalize=="C2"){
     A_star <- Xi[,1] * Pi
-
-
+    V=cbind(rep(1,K),V)
     #Step  5b: normalize each column to have a unit l1 norm
     temp <- colSums(A_star)
     A_hat <- t(apply(A_star,1,function(x) x/temp))
@@ -199,7 +199,18 @@ score <- function(D, K, scatterplot=FALSE, K0=NULL, m=NULL, M=NULL, threshold=FA
 
 
 
-
+core_recovery<- function(D,Xi1,Xi2,Xi3,V1,V2,V3,A_star3){
+  S_hat= tensor_create(D,t(Xi1),t(Xi2),t(Xi3))
+  a_0=colSums(abs(A_star3))
+  check_V3=a_0 *V3
+  G=tensor_create(S_hat,V1,V2,check_V3)
+  G3=matrization_tensor(G,3)
+  G3 <- pmax(G3,matrix(0,dim(G3)[1],dim(G3)[2])) ### sets negative entries to 0
+  temp <- colSums(G3)
+  G3 <- apply(G3,1,function(x) x/temp)
+  Gnew=tensorization(G3,3,dim(G)[1],dim(G)[2],dim(G)[3])
+  return(list(G=Gnew))
+}
 
 
 
