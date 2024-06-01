28028d7fbec26f801f21cc2fe4273102c1a69314
Yating Liu <13189092548@163.com>
2024-06-01 09:34:53 -0500

    modify

--- real_data/CNN.R
+++ real_data/CNN.R
@@ -1,5 +1,9 @@
 library(readr)
 source("D:yatingliu/CODE/tensor-topic-modeling/our_method.R")
+source("C:/Users/yichugang/Desktop/tensor-topic-modeling/VH_algo.R")
+source("C:/Users/yichugang/Desktop/tensor-topic-modeling/run_experiments.R")
+source("C:/Users/yichugang/Desktop/tensor-topic-modeling/data_generation.R")
+source("C:/Users/yichugang/Desktop/tensor-topic-modeling/our_method.R")
 CNN_Articels_clean <- read_csv("D:yatingliu/CNN_Articels_clean.csv/CNN_Articels_clean.csv")
 CNN=CNN_Articels_clean
 CNN$date=as.Date(CNN$`Date published`)
@@ -107,7 +111,7 @@ us_china_during_covid<- contain_us_china %>%
 
 
 during_covid<- CNN %>%
-  filter(Category %in% c("business","politics","sport","news"),
+  filter(Category %in% c("business","politics","sport","health"),
          date >as.Date("2019-12-31"),
          Index %in% contain_COVID$Index )%>%
   group_by(Category)%>%
@@ -115,18 +119,27 @@ during_covid<- CNN %>%
   ungroup()%>% arrange(Category,Section,date)
 
 
+before_covid<- CNN %>%
+  filter(Category %in% c("news"),
+         date <as.Date("2019-12-31"),
+         !Index %in% contain_COVID$Index )%>%
+  group_by(Category)%>%
+  sample_n(size=600,replace=FALSE)%>%
+  ungroup()%>% arrange(Category,Section,date)
+
+
 CNN_selected=c()
 # Define the number of rows to alternate and total rows in each dataframe
 chunk_size <- 5
-total_rows <- nrow(us_before_covid)  # Assuming dfA and dfB have the same number of rows
+total_rows <- nrow(during_covid)#nrow(us_before_covid)  # Assuming dfA and dfB have the same number of rows
 
 # Loop through each chunk
 
 for (i in seq(1, total_rows, by = chunk_size)) {
   # Bind 10 rows from dfA
-  CNN_selected <- rbind(CNN_selected, us_china_before_covid[i:(i + chunk_size - 1), ])
+  CNN_selected <- rbind(CNN_selected, before_covid[i:(i + chunk_size - 1), ])
   # Bind 10 rows from dfB
-  CNN_selected <- rbind(CNN_selected, us_china_during_covid[i:(i + chunk_size - 1), ])
+  CNN_selected <- rbind(CNN_selected, during_covid[i:(i + chunk_size - 1), ])
 }
 
 selected_data=CNN_selected
@@ -160,10 +173,13 @@ df <- data.frame(term = names(term_freq_select), freq = term_freq_select)
 df <- df[order(-df$freq), ]
 
 dtm_matrix<- dtm_matrix[,term_freq >= 5 & term_freq <=8000]
+sum(colSums(dtm_matrix)==0)
 dim(dtm_matrix)
+dtm_matrix=dtm_matrix/(selected_data$word_count)
 
-
-dtm_tensor=tensorization(t(dtm_matrix/(selected_data$word_count)),3,Q1=96,Q2=10,Q3=dim(dtm_matrix)[2])
+Q1=120
+Q2=10
+dtm_tensor=tensorization(t(dtm_matrix),3,Q1=Q1,Q2=Q2,Q3=dim(dtm_matrix)[2])
 
 
 
@@ -171,31 +187,32 @@ dtm_tensor=tensorization(t(dtm_matrix/(selected_data$word_count)),3,Q1=96,Q2=10,
 means_M=mean(selected_data$word_count)
 K1=4
 K2=2
-K3=2
-TTM=score(dtm_tensor,K1=K1,K2=K2,K3=K3,M=means_M,normalize="Ours")
+K3=4
+TTM=score(dtm_tensor,K1=K1,K2=K2,K3=K3,M=means_M,normalize="Ours",threshold = FALSE)
 hatA1=TTM$hatA1
 hatA2=TTM$hatA2
 hatA3=TTM$hatA3
 hatcore=TTM$hatcore
 hatcore_matrix=matrization_tensor(hatcore,3)
 
-heatmap_matrix(TTM$hatA1)
-heatmap_matrix(TTM$hatA2)
-heatmap_matrix(TTM$hatA3)
+heatmap_matrix(TTM$hatA1,"Groups","Mode 1")
+heatmap_matrix(TTM$hatA2,"Categories","Mode 2")
+heatmap_matrix(TTM$hatA3,"Topics","Mode 3")
 hatA3_2=TTM$hatA3
 rownames(hatA3)<-colnames(dtm_matrix)
-plot_words_per_group(hatA3,words=20) #
-plot_slice(TTM$hatcore@data,2)
+plot_words_per_group(hatA3,words=10) #
+plot_slice(TTM$hatcore@data,2,"Groups","Topics",TRUE)
 
 
 
-LDA_results=LDA(dtm_matrix,k=4,control = list(seed = 1234), method = 'VEM')
+matrix=floor(dtm_matrix*(selected_data$word_count))
+LDA_results=LDA(matrix,k=4,control = list(seed = 1234), method = 'VEM')
 lda_A=exp(t(LDA_results@beta))
 lda_W=t(LDA_results@gamma)
-rownames(lda_A)<-colnames(dtm_matrix)
-plot_words_per_group(lda_A,words=10) 
-heatmap_matrix(lda_A)
-heatmap_matrix(t(lda_W))
+rownames(lda_A) <-colnames(dtm_matrix)
+plot_words_per_group((lda_A),words=10) 
+heatmap_matrix(as.numeric(lda_A),"Groups","Mode 1")
+heatmap_matrix(t(lda_W),"Groups","Mode 1")
 
 
 ###
@@ -216,3 +233,36 @@ heatmap_matrix(t(NTF_result$A[[1]]))
 
 heatmap_matrix(t(NTF_result$A[[2]]))
 heatmap_matrix(t(NTF_result$A[[3]]))
+
+library(tidyverse)
+
+plot_words_per_group<- function(matrix,words=10){
+  #colnames(matrix) <- paste0("Group","_", colnames(matrix) )
+  gene_data <- as.data.frame(matrix) %>%
+    rownames_to_column(var = "Names") %>%
+    pivot_longer(cols = -Names, names_to = "Group", values_to = "Probability")
+  
+  top_genes <- gene_data  %>%
+    group_by(Group)%>%
+    top_n(n = words, wt = Probability) %>%
+    ungroup()
+  
+  # Plot
+  ggplot(top_genes, aes(x = Group, y = Names, size = Probability)) +
+    geom_point(shape = 21, fill = "skyblue",alpha = 0.6) +  # Adjust alpha for transparency, if desired
+    scale_size_continuous(range = c(0.1, 5)) +  # Adjust the size range for bubbles
+    theme_minimal() +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1)) +  # Rotate x-axis labels for better readability
+    labs(title = "",
+         x = "Group_Category",
+         y = "",
+         size = "Probability")
+}
+
+
+library(rdist)
+
+normalize_rows <- function(mat) {
+  row_norms <- sqrt(rowSums(mat^2))
+  return(sweep(mat, 1, row_norms, FUN="/"))
+}
