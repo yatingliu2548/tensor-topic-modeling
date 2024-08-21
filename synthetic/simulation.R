7efd6ab92ef5c44b3cc8d349ea742c0caf26b920
Yating Liu <13189092548@163.com>
2024-08-21 17:52:36 -0400

    clean synthetic

--- synthetic/simulation.R
+++ synthetic/simulation.R
@@ -1,7 +1,11 @@
 ##### more synthetic experiment
-source("D:/yatingliu/CODE/tensor-topic-modeling/our_method.R")
-set.seed(1234)
-D= t(rdiric(2,rep(0.8,50)))
+source("C:/Users/建新/Desktop/tensor-topic-modeling/tensor-topic-modeling/our_method.R")
+source("C:/Users/建新/Desktop/tensor-topic-modeling/tensor-topic-modeling/data_generation.R")
+
+source("C:/Users/建新/Desktop/tensor-topic-modeling/tensor-topic-modeling/VH_algo.R")
+library(nnTensor)
+set.seed(2024)
+D=t(rdiric(2,rep(0.8,50)))
 data=matrix(0,50,300)
 data[,1:15]=matrix(rep(D[,1],times=15),nrow=50)
 data[,16:30]=matrix(rep(D[,2],times=15),nrow=50)
@@ -12,10 +16,10 @@ data[,121:150]=data[,1:30]
 
 data[,151:300]=data[,1:150]
 
-before=rdiric(1,rep(0.8,50))#rdiric(1,rep(0.5,25))*sum(data[1:25,1])
-before2=rdiric(1,rep(0.5,25))*sum(data[1:25,16])
+before=rdiric(1,rep(0.9,50))#rdiric(1,rep(0.5,25))*sum(data[1:25,1])
+#before2=rdiric(1,rep(0.5,25))*sum(data[1:25,16])
 
-#data[26:50,1:15]=matrix(rep(before2,times=15),nrow=25)
+#data[1:25,16:30]=matrix(rep(before2,times=15),nrow=25)
 data[,16:30]=matrix(rep(before,times=15),nrow=50)
 data[,31:60]=data[,1:30]
 data[,61:90]=data[,1:30]
@@ -23,7 +27,7 @@ data[,91:120]=data[,1:30]
 data[,121:150]=data[,1:30]
 data1=data
 tensor1=array(t(data1),dim=c(30,10,50))
-plot_slice(tensor1,2)
+plot_slice(tensor1,2,"class","mode",option="viridis",limits=c(0,0.1))
 
 data[,11:20]=data[,1:10]*0.5+data[,21:30]*0.5
 data[,31:60]=data[,1:30]
@@ -40,7 +44,7 @@ data[,271:300]=data[,151:180]
 
 tensor_true=array(t(data),dim=c(30,10,50))
 Y_true=as.tensor(tensor_true)
-plot_slice(tensor_true,2,"Mode 1","Mode 3")
+plot_slice(tensor_true,2,"Mode 1","Mode 3",option="viridis",limits=c(0,0.1))
 
 D0=as.tensor(tensor_true)
 D3=matrization_tensor(D0,3)
@@ -64,13 +68,17 @@ tensor=Y@data
 #datanoise=datanoise/colSums(datanoise)
 
 #tensor=array(t(datanoise),dim=c(30,10,50))
-plot_slice(tensor,2)
+plot_slice(tensor,2,"mode1","mode2")
 Y=as.tensor(tensor)
 Y3=matrization_tensor(Y,3) # first 10 is dim2* first from dim 1
 
-heatmap_matrix(t(Y3))
+write.csv(Y3, file = "Y3.csv", row.names = F)
+write.csv(D3, file = "D3.csv", row.names = F)
+
+heatmap_matrix(t(Y3),"Topics","Mode 3")
+
 ##nonnegative Tucker decomposition
-NTD_result=NTD(Y/M,rank=c(2,2,4),algorithm="KL",nmf.algorithm = "KL")
+NTD_result=NTD(Y/M,rank=c(2,2,4),algorithm="KL")
 
 NTD_A1=t(NTD_result$A$A1)
 NTD_A1=NTD_A1/rowSums(NTD_A1)
@@ -93,7 +101,7 @@ hatY=tensor_create(NTD_G,NTD_A1,NTD_A2,NTD_A3)
 print(l1_error(hatY@data,D0@data))
 
 ## nonnegative CP decomposition
-NTF_result=cp(Y,num_components =3)
+NTF_result=cp(Y,num_components =4)
 U1=abs(NTF_result$U[[1]])
 U2=abs(NTF_result$U[[2]])
 U3=abs(NTF_result$U[[3]])
@@ -104,12 +112,13 @@ heatmap_matrix2(U2/rowSums(U2),"Topics","Mode 2")
 heatmap_matrix(U3/colSums(U3),"Topics","Mode 3")
 print(NTF_result$S)
 
-A=t(NTF_result$A[[1]])
-B=t(NTF_result$A[[2]])
+A=t(NTF_result$U[[1]])
+B=t(NTF_result$U[[2]])
 C=t(NTF_result$A[[3]])
-core_values=NTF_result$S
+core_values=NTF_result$lambdas
+YY=get_cp(U1/rowSums(U1),U2/rowSums(U2),U3/colSums(U3),NTF_result$lambdas)
 
-print(l2_error(get_cp(A,B,C,core_values),Ytrue@data))
+print(l1_error(YY,D0@data))
 
 ## matrix
 M=100
@@ -138,7 +147,7 @@ hatY=tensor_create(ours_results$hatcore,ours_results$hatA1,ours_results$hatA2,ou
 print(l1_error(hatY@data,D0@data))
 
 
-heatmap(t(data), 
+heatmap(t(data),
         Rowv = NA, Colv = NA, # Disable clustering
         col = heat.colors(256), # Color scheme
         scale = "none", # Do not scale rows
@@ -160,7 +169,7 @@ for (i in 1:30){
   lda_A=exp(t(LDA_results@beta))
   lda_W=t(LDA_results@gamma)
   hatY=lda_A%*%lda_W
- 
+
   errors=rbind(errors,error_sim(D0@data,tensorization(hatY,3,30,10,50)@data,"LDA"))
   ours_results=score(Y/M,K1=2,K2=2,K3=4,M=M,normalize="Ours")
   our_D=tensor_create(ours_results$hatcore,ours_results$hatA1,ours_results$hatA2,ours_results$hatA3)
@@ -168,7 +177,7 @@ for (i in 1:30){
 }
 
 
-ggplot(errors %>% filter(method %in% c("NTD","ours","LDA")), 
+ggplot(errors %>% filter(method %in% c("NTD","ours","LDA")),
        aes(x=method,y=l1,fill=method))+
   geom_boxplot()+
   theme_minimal()
@@ -177,9 +186,10 @@ ggplot(errors %>% filter(method %in% c("NTD","ours","LDA")),
 
 library(ggplot2)
 library(reshape2) # For melt function
+library(viridis)
+library(scico)
 
-
-plot_slice <- function(tensor, k=1,xlab,ylab,yes=FALSE) {
+plot_slice <- function(tensor, k=1,xlab,ylab,yes=FALSE,option="H",limits=c(0,1)) {
   df_long <- data.frame()
   # Extract a slice from the 2nd dimension
   # Convert each slice (along the second dimension) of the tensor to long format
@@ -195,13 +205,13 @@ plot_slice <- function(tensor, k=1,xlab,ylab,yes=FALSE) {
       mutate(X = row_number(), Slice = i) %>%
       pivot_longer(cols = -c(X, Slice), names_to = "Y", values_to = "Value") %>%
       mutate(Y = as.numeric(gsub("V", "", Y))) # Convert Y to numeric
-    
+
     df_long <- rbind(df_long, slice_df) # Bind each slice's data frame to the main data frame
   }
-  
+
   # Adjust factors for plotting
   df_long$Slice <- factor(df_long$Slice)
-  
+
   if (yes==TRUE){
     df_long$X=as.factor(df_long$X)
     df_long$Y=as.factor(df_long$Y)
@@ -209,15 +219,15 @@ plot_slice <- function(tensor, k=1,xlab,ylab,yes=FALSE) {
   # Plotting
   g=ggplot(df_long, aes(x = X, y =Y, fill = Value)) +
     geom_tile() +  # Create the heatmap
-    scale_fill_viridis_c(limits=c(0,1),option="H",trans="sqrt") +  # Use a color scale that's visually appealing for heatmaps
+    scale_fill_viridis_c(limits=limits,option=option,trans="sqrt") +  # Use a color scale that's visually appealing for heatmaps
     facet_wrap(~Slice, ncol = 10) +  # Facet by slice, adjust ncol for desired layout
     labs(x = xlab, y = ylab, fill = "") +
     theme_minimal() +  # Use a minimal theme
     theme(axis.text.x = element_text(angle = 45, hjust = 1), # Improvehttp://127.0.0.1:18569/graphics/5001c0ec-4c59-4d8d-8e0d-a42a99b7917e.png x-axis label readability
           #strip.background = element_rect(fill = "lightblue"), # Customize facet label background
           strip.text = element_text(face = "bold"))  # Bold facet labels
-  
-  
+
+
   print(g)
 }
 
@@ -228,20 +238,20 @@ heatmap_matrix<- function(matrix_data,xlab,ylab){
     rownames_to_column("Row") %>%
     pivot_longer(cols = -Row, names_to = "Column", values_to = "Value") %>%
     mutate(Row = as.numeric(Row), Column = as.numeric(gsub("V", "", Column))) # Convert Row and Column to numeric
-  
+
   # Create heatmap
   g=ggplot(df, aes(x = as.factor(Column), y =( Row), fill = Value)) +
     geom_tile() +
-    scale_fill_viridis_c(limits=c(0,1),option="H",trans="sqrt") +  # Use a color scale that's visually appealing for heatmaps
+    scale_fill_viridis_c(limits=c(0,1),option="H") +  # Use a color scale that's visually appealing for heatmaps
     # scale_fill_viridis_c() +  # Us
     labs(x = xlab, y = ylab, fill = "") +
     #scale_y_continuous(breaks=seq(0,max(as.numeric(df$Row)),by=5))+
     theme_minimal() +  # Use a minimal theme
     theme(#axis.text.x = element_text(angle = 45, hjust = 1), # Improve x-axis label readability
-          strip.background = element_rect(fill = "lightblue"), # Customize facet label background
+          strip.background = element_rect(fill = "red"), # Customize facet label background
           strip.text = element_text(face = "bold"))  # Bold facet labels
-  
-  
+
+
   print(g)
 }
 heatmap_matrix2<- function(matrix_data,xlab,ylab){
@@ -250,11 +260,11 @@ heatmap_matrix2<- function(matrix_data,xlab,ylab){
     rownames_to_column("Row") %>%
     pivot_longer(cols = -Row, names_to = "Column", values_to = "Value") %>%
     mutate(Row = as.numeric(Row), Column = as.numeric(gsub("V", "", Column))) # Convert Row and Column to numeric
-  
+
   # Create heatmap
   g=ggplot(df, aes(x = as.factor(Column), y =( Row), fill = Value)) +
     geom_tile() +
-    scale_fill_viridis_c(limits=c(0,1),option="H",trans="sqrt") +  # Use a color scale that's visually appealing for heatmaps
+    scale_fill_viridis_c(limits=c(0,1),option="H") +  # Use a color scale that's visually appealing for heatmaps
     # scale_fill_viridis_c() +  # Us
     labs(x = xlab, y = ylab, fill = "") +
     scale_y_continuous(breaks=seq(0,max(as.numeric(df$Row)),by=5))+
@@ -262,8 +272,8 @@ heatmap_matrix2<- function(matrix_data,xlab,ylab){
     theme(#axis.text.x = element_text(angle = 45, hjust = 1), # Improve x-axis label readability
       strip.background = element_rect(fill = "lightblue"), # Customize facet label background
       strip.text = element_text(face = "bold"))  # Bold facet labels
-  
-  
+
+
   print(g)
 }
 
@@ -275,10 +285,10 @@ error_sim <- function(D,hatD,method=NULL,i){
 }
 
 get_cp=function(A,B,C,core_values){
- 
+
   tensor_dims <- c(nrow(A), nrow(B), nrow(C))
   cp_tensor <- array(0, dim = tensor_dims)
- 
+
   for (r in 1:length(core_values)) {
     rank_one_tensor <- outer(A[, r], B[, r])
     rank_one_tensor <- array(apply(rank_one_tensor, 1:2, function(x) outer(x, C[, r])), dim = tensor_dims)
@@ -286,3 +296,4 @@ get_cp=function(A,B,C,core_values){
   }
   return(cp_tensor)
 }
+
